eacb8fcb13f9dec79cd064a11b6080c02e8d25d8
Niccolo <nico.maffioli@gmail.com>
2025-04-04 00:04:01 +0200

    add: arrow animation

--- js/slider.js
+++ js/slider.js
@@ -1,38 +1,61 @@
 const slider = () => {
     document.addEventListener("DOMContentLoaded", () => {
         const sliders = document.querySelectorAll('.sliderContainer');
-    
+
         sliders.forEach(slider => {
-           /*  const paginations = slider.parentElement.querySelectorAll('.pagination'); */
             const leftArrow = slider.parentElement.querySelector('.arrow-left');
             const rightArrow = slider.parentElement.querySelector('.arrow-right');
-    
-            /* let scrollAmount = 0; */
+
+            console.log("scrollLeft: ", slider.scrollLeft);
+            console.log("clientWidth", slider.clientWidth);
+            console.log("scrollWidth", slider.scrollWidth);
+
             const scrollStep = slider.clientWidth * 1.0;
-    
+
+            const updateArrows = () => {
+                if (slider.scrollLeft <= 0) {
+                    leftArrow.style.opacity = "0.5"; 
+                    leftArrow.style.pointerEvents = "none"; 
+                } else {
+                    leftArrow.style.opacity = "1";
+                    leftArrow.style.pointerEvents = "auto";
+                }
+            
+                
+                if (slider.scrollWidth >= slider.clientWidth) {
+                    if (slider.scrollLeft >= 1500) {
+                        rightArrow.style.opacity = "0.5";
+                        rightArrow.style.pointerEvents = "none";
+                    } else {
+                        rightArrow.style.opacity = "1";
+                        rightArrow.style.pointerEvents = "auto";
+                    }
+                } else {
+                    
+                    rightArrow.style.opacity = "0.5";
+                    rightArrow.style.pointerEvents = "none";
+                }
+            };
+
             rightArrow.addEventListener("click", () => {
                 slider.scrollBy({ left: scrollStep, behavior: "smooth" });
-    
-                const activePagination = document.querySelector('.active-pagination');
-    
-                let nextPagination = activePagination.nextElementSibling;
-    
-                activePagination.classList.remove("active-pagination");
-                nextPagination.classList.add("active-pagination");
+                setTimeout(updateArrows, 500);
+                console.log("scrollLeft: ", slider.scrollLeft);
+                console.log("clientWidth", slider.clientWidth);
+                console.log("scrollWidth", slider.scrollWidth);
             });
-    
+
             leftArrow.addEventListener("click", () => {
                 slider.scrollBy({ left: -scrollStep, behavior: "smooth" });
-    
-                const activePagination = document.querySelector('.active-pagination');
-    
-                let prevPagination = activePagination.previousElementSibling;
-    
-                activePagination.classList.remove("active-pagination");
-                prevPagination.classList.add("active-pagination");
+                setTimeout(updateArrows, 500);
+                console.log("scrollLeft: ", slider.scrollLeft);
+                console.log("clientWidth", slider.clientWidth);
+                console.log("scrollWidth", slider.scrollWidth);
             });
-        });
-    });    
-}
 
-export default slider
+            updateArrows();
+        });
+    });
+};
+
+export default slider;
