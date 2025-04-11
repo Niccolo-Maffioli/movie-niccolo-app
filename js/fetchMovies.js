7cefa2688e1884e9ea1022286b733a7a959c1b1b
Niccolo <nico.maffioli@gmail.com>
2025-04-11 17:54:16 +0200

    feat: add try and catch

--- js/fetchMovies.js
+++ js/fetchMovies.js
@@ -1,12 +1,30 @@
 const fetchFromTMDB = async (type, category, page) => {
-    const result = await fetch(`https://api.themoviedb.org/3/${type}/${category}?page=${page}`, {
-        headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGRkZjNlMjc0YmE3MzdiYjA0YzIwNmJhYmVjNWZlYSIsIm5iZiI6MTY1NTU2Nzg2NC4wNDMsInN1YiI6IjYyYWRmNWY4OTBlYTRiMDA1NDNlZDVmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZnxQ4OCckJJgFaHl065UbxyUk1_R4FJH82V8T3NiCYw'
+    try {
+        if (isNaN(page)) {
+            throw new Error("La pagina deve essere un numero valido.");
         }
-    });
 
-    const data = await result.json();
-    return data;
+        if (typeof category !== "string") {
+            throw new Error("Il parametro 'category' deve essere una stringa.");
+        }
+
+        if (typeof type !== "string") {
+            throw new Error("Il parametro 'type' deve essere una stringa.");
+        }
+
+        const result = await fetch(`https://api.themoviedb.org/3/${type}/${category}?page=${page}`, {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGRkZjNlMjc0YmE3MzdiYjA0YzIwNmJhYmVjNWZlYSIsIm5iZiI6MTY1NTU2Nzg2NC4wNDMsInN1YiI6IjYyYWRmNWY4OTBlYTRiMDA1NDNlZDVmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZnxQ4OCckJJgFaHl065UbxyUk1_R4FJH82V8T3NiCYw'
+            }
+        });
+
+        const data = await result.json();
+        return data;
+
+    } catch (error) {
+        console.error("Errore nella fetch:", error.message);
+        throw error;
+    }
 };
 
 export default fetchFromTMDB;
