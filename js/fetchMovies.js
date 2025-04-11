ff6cdfaa381c7755f3ebbc57d832ca71c6449b0a
Niccolo <nico.maffioli@gmail.com>
2025-04-11 11:52:52 +0200

    style update

--- js/fetchMovies.js
+++ js/fetchMovies.js
@@ -1,5 +1,5 @@
 const getMovie = async () => {
-        const result = await fetch('https://api.themoviedb.org/3/movie/popular?language=it-IT&page=1', {
+        const result = await fetch('https://api.themoviedb.org/3/movie/popular?&page=1', {
             headers: {
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGRkZjNlMjc0YmE3MzdiYjA0YzIwNmJhYmVjNWZlYSIsIm5iZiI6MTY1NTU2Nzg2NC4wNDMsInN1YiI6IjYyYWRmNWY4OTBlYTRiMDA1NDNlZDVmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZnxQ4OCckJJgFaHl065UbxyUk1_R4FJH82V8T3NiCYw'
             }
